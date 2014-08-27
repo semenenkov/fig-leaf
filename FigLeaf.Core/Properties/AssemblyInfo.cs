73c17ea6075f5a6796651fc8273f0a6cdc7b857c
semenenkov <semenenkov.k@gmail.com>
2014-08-27 15:39:06 +0400

    SharedAssemblyInfo with shared version, author, etc

--- FigLeaf.Core/Properties/AssemblyInfo.cs
+++ FigLeaf.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,11 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FigLeaf.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Konstantin Semenenkov")]
-[assembly: AssemblyProduct("FigLeaf.Core")]
-[assembly: AssemblyCopyright("Copyright © Konstantin Semenenkov 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2f973ea2-1aa4-4bc5-992f-b8786f7b0bc1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
