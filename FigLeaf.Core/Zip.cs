463d69a8212cd6df57c194c0e1e3ca418304d297
semenenkov <semenenkov.k@gmail.com>
2014-09-03 18:50:10 +0400

    Fixed extraction of files with Cyrillic names (and any other names transformed inside zip)

--- FigLeaf.Core/Zip.cs
+++ FigLeaf.Core/Zip.cs
@@ -22,18 +22,41 @@ namespace FigLeaf.Core
 
 		public bool Unpack(string sourcePath, FileInfo targetFile)
 		{
-			var zip = new FastZip();
-			zip.Password = _passwordGenerator.GetPassword(targetFile.Name);
 			try
 			{
-				zip.ExtractZip(sourcePath, targetFile.Directory.FullName, FastZip.Overwrite.Prompt, name => true, "", "", true);
+				using (FileStream fs = File.OpenRead(sourcePath))
+				{
+					var zipFile = new ZipFile(fs);
+					zipFile.Password = _passwordGenerator.GetPassword(targetFile.Name);
+
+					foreach (ZipEntry zipEntry in zipFile)
+					{
+						if (!zipEntry.IsFile)
+							continue;
+
+						using (Stream zipStream = zipFile.GetInputStream(zipEntry))
+						using (FileStream extractStream = File.Create(targetFile.FullName))
+							CopyStream(zipStream, extractStream);
+
+						// expect to have one file per zip, target file is unique
+						break;
+					}
+				}
 				return true;
 			}
 			catch
 			{
-				// most likely it is a thumbnail
+				// most likely it is a thumbnail, not a zip
 				return false;
 			}
+		}
+
+		public static void CopyStream(Stream sourceStream, Stream targetStream)
+		{
+			var buffer = new byte[0x10000];
+			int n;
+			while ((n = sourceStream.Read(buffer, 0, buffer.Length)) != 0)
+				targetStream.Write(buffer, 0, n);
 		}
 	}
 }
